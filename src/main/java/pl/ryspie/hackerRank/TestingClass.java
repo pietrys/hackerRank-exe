3d0fd26b733fcb5171b60fcb978a235c5a44f7d8
Ryszard P <Riczm0ndes@gmail.com>
2022-08-11 18:30:33 +0200

    first testing commit

---
+++ src/main/java/pl/ryspie/hackerRank/TestingClass.java
@@ -0,0 +1,10 @@
+package pl.ryspie.hackerRank;
+
+public class TestingClass {
+    public static void main(String[] args) {
+        int a =4;
+        int b =4;
+
+        System.out.println(a+b);
+    }
+}
